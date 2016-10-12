2de8d28db91a45cf97b6c5fe9715fd39e02faf0f
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-10-12 20:10:42 +0200

    FIX: MoodleDownload

--- src/main/java/ch/chiodo/sssync/sync/MoodleDownload.java
+++ src/main/java/ch/chiodo/sssync/sync/MoodleDownload.java
@@ -1,12 +1,15 @@
 package ch.chiodo.sssync.sync;
 
+import ch.chiodo.sssync.configuration.Entity.EncryptedString;
+
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
+import java.security.KeyStoreException;
 
 public class MoodleDownload implements Download {
 
     @Override
-    public void StartDownload(String source, String destination) throws DownloadException, MalformedURLException, UnknownHostException {
+    public void StartDownload(String source, String destination, EncryptedString password) throws DownloadException, MalformedURLException, UnknownHostException, KeyStoreException {
 
     }
 }
